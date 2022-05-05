a5178ee738277f4b7ac4f91fb09eea076fab5482
GiampaoloFortunato <giampaolo.fortunato@gmail.com>
2022-05-05 22:32:03 +0200

    Finita funzione click

--- progetti/carrello spesa/carrello.js
+++ progetti/carrello spesa/carrello.js
@@ -10,10 +10,31 @@ let car = {
 
 let elem;
 
-
 window.onload = function(){
     elem = document.getElementsByTagName("button");
     for(let i = 0; i < elem.length; i++){
-        elem[i].addEventListener("click", ()=>i);
+         elem[i].addEventListener("click",()=>visualizzaCarrello(i));
+    }
+}
+
+
+function visualizzaCarrello(index){
+    aggiungiProdotto(index);
+    visualizzaNuovoProdotto();
+    stampaPrezzo();
+}
+
+function aggiungiProdotto(index){
+    let prodotto = {id:index, name: document.getElementsByTagName("h3")[index].innerText,price: parseFloat(document.getElementsByClassName("price")[index].innerText)};
+    car.add(prodotto);
+}
+
+function visualizzaNuovoProdotto(){
+    document.getElementById("lista").innerHTML += "<li> <p>" + car.shoppingBag[car.shoppingBag.length-1].name + "</p><div class='costo'>"+ car.shoppingBag[car.shoppingBag.length-1].price + "</div>"  + "</li>";
+}
+
+function stampaPrezzo(){
+    if(car.shoppingBag.length != 0){
+        document.getElementById("totalPrice").innerHTML = "<p>" + car.totalPrice() +"</p>";
     }
 }
