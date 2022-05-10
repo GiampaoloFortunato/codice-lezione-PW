225be7e15ad55370176a2cbb713317148f215475
GiampaoloFortunato <giampaolo.fortunato@gmail.com>
2022-05-10 11:46:27 +0200

    aggiunta funzione cancella da carrello

--- progetti/carrello spesa/carrello.js
+++ progetti/carrello spesa/carrello.js
@@ -3,20 +3,35 @@ let car = {
     add: function(prodotto){
         this.shoppingBag.push(prodotto);
     },
+    remove: function(name){
+        for(let i= 0; i < this.shoppingBag.length; i++){
+            if(this.shoppingBag[i].name === name){
+                this.shoppingBag.splice(i, 1);
+                return;
+            }
+        }
+    },
     totalPrice: function(){
         return this.shoppingBag.reduce((acc, item) => acc + item.price, 0);
     }
 };
 
-let elem;
+let elemAdd;
+let elemDelete;
 
-window.onload = function(){
-    elem = document.getElementsByTagName("button");
-    for(let i = 0; i < elem.length; i++){
-         elem[i].addEventListener("click",()=>visualizzaCarrello(i));
-    }
+window.onload = function(){  
+    elemAdd = document.getElementsByClassName("add");
+    elemDelete = document.getElementsByClassName("delete");
+    fCicloAdd();
+    fCicloDelete();
+   
 }
 
+function fCicloAdd(){
+    for(let i = 0; i < elemAdd.length; i++){
+        elemAdd[i].addEventListener("click",() => visualizzaCarrello(i));
+    }
+}
 
 function visualizzaCarrello(index){
     aggiungiProdotto(index);
@@ -25,16 +40,37 @@ function visualizzaCarrello(index){
 }
 
 function aggiungiProdotto(index){
-    let prodotto = {id:index, name: document.getElementsByTagName("h3")[index].innerText,price: parseFloat(document.getElementsByClassName("price")[index].innerText)};
-    car.add(prodotto);
+    car.add(
+        {id:index, name: document.getElementsByTagName("h3")[index].innerText,price: parseFloat(document.getElementsByClassName("price")[index].innerText)}
+    );
 }
 
 function visualizzaNuovoProdotto(){
-    document.getElementById("lista").innerHTML += "<li> <p>" + car.shoppingBag[car.shoppingBag.length-1].name + "</p><div class='costo'>"+ car.shoppingBag[car.shoppingBag.length-1].price + "</div>"  + "</li>";
+    document.getElementById("lista").innerHTML += "<li class='prodList'> <p>" + car.shoppingBag[car.shoppingBag.length-1].name + "</p><div class='costo'>"+ car.shoppingBag[car.shoppingBag.length-1].price + "</div><button class='delete'>x</button></li>";
 }
 
 function stampaPrezzo(){
     if(car.shoppingBag.length != 0){
         document.getElementById("totalPrice").innerHTML = "<p>" + car.totalPrice() +"</p>";
+    }
+}
+
+//Non funziona se premo il pulsante, ma la funzione è corretta, in quanto quando la eseguo da console si comporta nel modo previsto.
+function fCicloDelete(){
+    for(let i = 0; i < car.shoppingBag.length; i++){
+        elemDelete[i].addEventListener("click", () => cancellaDaCarello(i));
+    }
+}
+
+function cancellaDaCarello(index){
+    car.remove(car.shoppingBag[index].name);
+    visualizzaProdotti();
+    stampaPrezzo();
+}
+function visualizzaProdotti(){
+    let carrello = document.getElementById("lista");
+    carrello.innerHTML = "";
+    for(let i = 0; i < car.shoppingBag.length ; i++){
+        carrello.innerHTML += "<li class='prodList'> <p>" + car.shoppingBag[i].name + "</p><div class='costo'>"+ car.shoppingBag[i].price + "</div><button class='delete'>x</button></li>";
     }
 }
