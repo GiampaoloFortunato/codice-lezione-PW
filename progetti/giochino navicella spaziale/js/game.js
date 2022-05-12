0f35e61e7713248fe0a048150f84fd41fd8bec16
GiampaoloFortunato <giampaolo.fortunato@gmail.com>
2022-05-12 22:59:48 +0200

    minigioco: aggiunto nemici

--- progetti/giochino navicella spaziale/js/game.js
+++ progetti/giochino navicella spaziale/js/game.js
@@ -5,73 +5,118 @@
     canvas.width = 626;
     canvas.height = 500;
     document.body.appendChild(canvas);
+    let contex = canvas.getContext("2d");
 
-    let canvasContex = canvas.getContext("2d");
-
+    //Strutture dati per rappresentare personaggi
     let navicella = {x: parseInt(canvas.width/2)-24, y: parseInt(canvas.height)-60, speed: 256};
-    let fire = {x: parseInt(canvas.width/2)+24, y: parseInt(canvas.height)-16, speed: 200};
-    let attacco = [];
+    let fire = {x: parseInt(canvas.width/2)+24, y: parseInt(canvas.height)-16, speed: 300};
+    let enemy = {x:30, y:30};
+    let listFire = [];
+    let listEnemy = [];
     let score = 0;
+
     const requestAnimationFrame =
         window.requestAnimationFrame ||
         window.mozRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
         window.webkitRequestAnimationFrame;
 
+    //Caricamento skin e background
     const bgImage = new Image();
     bgImage.src = "img/spazio.webp";
     const imgNavicella = new Image();
     imgNavicella.src = "img/nav2.png";
     const imgFire = new Image();
     imgFire.src = "img/fire.png";
+    const imgEnemy = new Image();
+    imgEnemy.src =  "img/enemy.png";
     let bgFlag = false;
     let navFlag = false;
     let fireFlag = false;
+    let enemyFlag = false;
     bgImage.onload = () => bgFlag = true;
     imgNavicella.onload = () => navFlag = true;
     imgFire.onload = () => fireFlag = true;
+    imgEnemy.onload = () => enemyFlag = true;
     
+
     function viewScore(){
-        canvasContex.fillStyle = "#00ff90";
-        canvasContex.font = "18px Courier";
-        canvasContex.fillText("Score: " + score, 20, 20);
+        contex.fillStyle = "#00ff90";
+        contex.font = "18px Courier ";
+        contex.fillText("Score: " + score, 20, 20);
     }
     function render(){
         if(bgFlag)
-            canvasContex.drawImage(bgImage, 0, 0);
+            contex.drawImage(bgImage, 0, 0);
         if(navFlag)    
-            canvasContex.drawImage(imgNavicella, navicella.x, navicella.y);
+            contex.drawImage(imgNavicella, navicella.x, navicella.y);
         if(fireFlag){
-            attacco.forEach((el) => {
-                canvasContex.drawImage(imgFire, el.x, el.y);
+            listFire.forEach((el) => {
+                contex.drawImage(imgFire, el.x, el.y);
             })
+        }
+        if(enemyFlag){
+            listEnemy.forEach((el) => {
+            contex.drawImage(imgEnemy, el.x, el.y);
+            });
         }
         viewScore();
     }    
     
+    /*
+    * Struttura dati per memorizzare pressione dei tasti.
+    * Con "keydown" memorizziamo il/i tasto/i premuto/i
+    * Con "keyup" cancelliamo il tasto appena premuto, questo per come è stata pensata la funzione update(). 
+    * Se si vuole modificare questa struttura dati bisogna ripensare la funzione update.
+    */
     let keysDown = {};
     addEventListener("keydown", (e)=> keysDown[e.keyCode] = true);
     addEventListener("keyup", (e) => {delete keysDown[e.keyCode]});
 
     function update(modifier){
+        //37 corrisponde al pulsante "sinistra" 
         if(37 in keysDown && navicella.x > 0){
             navicella.x -= navicella.speed * modifier;
         }
+        //39 corrisponde al pulsante "destra"
         if(39 in keysDown && navicella.x < canvas.width-48){
             navicella.x += navicella.speed * modifier;
         }
+        //32 corrisponde alla spacebar
         if(32 in keysDown){
             fire.x = navicella.x+12;
             fire.y = parseInt(canvas.height)-16;
-            attacco[attacco.length] = Object.assign({}, fire);
+            listFire[listFire.length] = Object.assign({}, fire);
         }else{
-            attacco.forEach((el, index) => {
+            listFire.forEach((el, index) => {
                 if(el.y > 20){
                     el.y -= el.speed * modifier;
                 }else{
-                    attacco.splice(index,1);
+                    listFire.splice(index,1);
                 }
             });
+        }
+        createEnemy(4);
+        hit();
+    }
+    //controllo se ho colpito navicella avversaria
+    function hit(){
+        listFire.forEach((el) => {
+            listEnemy.forEach((en, index) => {
+                if (Math.abs(el.x - en.x) < 24 && Math.abs(el.y - en.y) < 24){
+                    score++;
+                    listEnemy.splice(index, 1);
+                }
+            })
+        })
+    }
+
+    function createEnemy(difficolta){
+        let rand = Math.random();
+        if(rand < difficolta * 10**-2){
+            let newEnemy = Object.assign({}, enemy);
+            newEnemy.x = parseInt(Math.random() * canvas.width);
+            newEnemy.y = parseInt(Math.random() * 200);
+            listEnemy[listEnemy.length] = newEnemy;
         }
     }
 
